cd8bd745cd73fc8a5c17ca1f87874ebaba7d65ab
dipa.alhaza <dipa.alhaza@ui.ac.id>
2025-07-27 14:38:07 +0700

    chore: adjust structure

--- internal/services/auth/app/di/wire.go
+++ internal/services/auth/app/di/wire.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/dpalhz/microservice-exp-with-go/internal/pkg/database"
 	"github.com/dpalhz/microservice-exp-with-go/internal/pkg/kafka"
+	"github.com/dpalhz/microservice-exp-with-go/internal/pkg/redisdb"
 	"github.com/dpalhz/microservice-exp-with-go/internal/services/auth/app"
 	"github.com/dpalhz/microservice-exp-with-go/internal/services/auth/event"
 	"github.com/dpalhz/microservice-exp-with-go/internal/services/auth/handler"
@@ -20,30 +21,27 @@ import (
 
 func InitializeApp(log *slog.Logger) (*app.App, func(), error) {
 	wire.Build(
-		// ⛓ BINDING INTERFACE TO IMPLEMENTATION — WAJIB!
 		wire.Bind(new(usecase.UserRepository), new(*repository.PostgresUserRepository)),
 		wire.Bind(new(usecase.EventProducer), new(*event.KafkaEventProducer)),
 		wire.Bind(new(usecase.TokenGenerator), new(*token.JWTGenerator)),
 		wire.Bind(new(usecase.SessionStore), new(*session.RedisSessionStore)),
-		wire.Bind(new(usecase.VerificationRepository), new(*repository.PostgresVerificationRepository)),
 
-		// ✅ PROVIDER CONSTRUCTORS
 		app.New,
-		handler.NewFiberHandler,
-		usecase.NewUserUsecase,
-		repository.NewPostgresUserRepository,
-		repository.NewPostgresVerificationRepository,
-		event.NewKafkaEventProducer,
-		token.NewJWTGenerator,
-		session.NewRedisSessionStore,
-		app.ProvideRedisClient,
-		database.NewPostgresDB,
-		kafka.NewProducer,
 		app.ProvideDBConfig,
 		app.ProvideKafkaConfig,
 		app.ProvideJWTConfig,
 		app.ProvideRedisConfig,
 		app.ProvideServerConfig,
+		handler.NewFiberHandler,
+		usecase.NewUserUsecase,
+		repository.NewPostgresUserRepository,
+		event.NewKafkaEventProducer,
+		token.NewJWTGenerator,
+		session.NewRedisSessionStore,
+		redisdb.NewClient,
+		database.NewPostgresDB,
+		kafka.NewProducer,
+	
 	)
 	return &app.App{}, nil, nil
 }
